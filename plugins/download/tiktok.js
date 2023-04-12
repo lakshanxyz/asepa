9275908cc9a08bdfe9ab11b78d9e31c859e90d93
Lakshan...! <108253179+lakshanxyz@users.noreply.github.com>
2023-04-11 16:19:20 -1200

    Update tiktok.js

--- plugins/download/tiktok.js
+++ plugins/download/tiktok.js
@@ -13,13 +13,13 @@ exports.run = {
    	try {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://vt.tiktok.com/ZS8CdhRnb/'), m)
          if (!args[0].match('tiktok.com')) return client.reply(m.chat, global.status.invalid, m)
-         client.sendReact(m.chat, '🕒', m.key)
+         client.sendReact(m.chat, '💚', m.key)
          let old = new Date()
          let json = await tiktok(Func.ttFixed(args[0]))
          if (Object.keys(json).length < 1) return client.reply(m.chat, global.status.fail, m)
          let caption = `乂 *Tiktok - Download*\n\n`
          caption += `	◦  *Sound* : ${json.title_audio}\n`
-         caption += `	◦  *Fetching* : ${((new Date - old) * 1)} ms\n\n`
+         caption += `	◦  * 💐Fetching* : ${((new Date - old) * 1)} ms\n\n`
          caption += global.footer
          if (command == 'tiktok' || command == 'tt') return client.sendMessage(m.chat, { video: { url: json.video[0] }, caption: caption }, { quoted: m })
          //client.sendFile(m.chat, json.video[0], '', caption, m)
