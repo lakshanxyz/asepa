0690a82257b4a81d109adfe8f247215e05a17813
Lakshan...! <108253179+lakshanxyz@users.noreply.github.com>
2023-04-11 16:18:35 -1200

    Update igs.js

--- plugins/download/igs.js
+++ plugins/download/igs.js
@@ -11,12 +11,12 @@ exports.run = {
    }) => {
       try {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://instagram.com/stories/pandusjahrir/3064777897102858938?igshid=MDJmNzVkMjY='), m)
-         client.sendReact(m.chat, '🕒', m.key)
+         client.sendReact(m.chat, '💜', m.key)
          let old = new Date()
          let json = await Api.igs(args[0])
          if (!json.status) return client.reply(m.chat, global.status.fail, m)
          for (let i = 0; i < json.data.length; i++) {
-            client.sendFile(m.chat, json.data[i].url, ``, `🍟 *Fetching* : ${((new Date - old) * 1)} ms (${i+1})`, m)
+            client.sendFile(m.chat, json.data[i].url, ``, `🍃 *Fetching* : ${((new Date - old) * 1)} ms (${i+1})`, m)
             await Func.delay(1500)
          }
       } catch (e) {
