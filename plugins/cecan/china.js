bdb05cdeab6acaa2ed8bfa7a1428f9b8a6f0225f
Lakshan...! <108253179+lakshanxyz@users.noreply.github.com>
2023-05-14 08:18:24 +0530

    Create china.js

---
+++ plugins/cecan/china.js
@@ -0,0 +1,21 @@
+exports.run = {
+  usage: ["cecan"],
+  hidden: ["china"],
+  use: "word",
+  category: "utilities",
+  async: async (m, { client, args, isPrefix, command, participants }) => {
+    try {
+      client.sendReact(m.chat, "🕒", m.key);
+      let old = new Date();
+      let json = await Func.fetchBuffer(`https://restapi.zeynnnn.repl.co/api/cecan/china?apikey=zeyn`)
+      client.sendFile(m.chat, json, '', `🍟 Fetching : ${((new Date - old) * 1)} ms`, m)
+      } catch (e) {
+         console.log(e)
+         return client.reply(m.chat, global.status.error, m)
+      }
+   },
+   error: false,
+   limit: true,
+   cache: true,
+   location: __filename
+}
