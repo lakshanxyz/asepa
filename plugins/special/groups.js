9ac2e812e4af406ea6507c93821e088b6bc4e3a6
Lakshan...! <108253179+lakshanxyz@users.noreply.github.com>
2023-05-16 22:41:35 +0530

    Update groups.js

--- plugins/special/groups.js
+++ plugins/special/groups.js
@@ -10,14 +10,14 @@ exports.run = {
       let groupList = async () => Object.entries(await client.groupFetchAllParticipating()).slice(0).map(entry => entry[1])
       let groups = await groupList()
       let rows = []
-      let caption = `乂  *G R O U P - L I S T*\n\n`
-      caption += `*“Bot has joined into ${groups.length} groups, send _${isPrefix}gc_ or _${isPrefix}gcopt_ to show all setup options.”*\n\n`
-      groups.map((x, i) => {
+      groups.map(x => {
          let v = global.db.groups.find(v => v.jid == x.id)
          if (v) {
-            caption += `›  *${(i + 1)}.* ${x.subject}\n`
-            caption += `   *💳* : ${x.id.split`@`[0]}\n`
-            caption += `${v.stay ? '   FOREVER' : (v.expired == 0 ? '   NOT SET' : '   ' + Func.timeReverse(v.expired - new Date() * 1))} | ${x.participants.length} | ${(v.mute ? 'OFF' : 'ON')} | ${moment(v.activity).format('DD/MM/YY HH:mm:ss')}\n\n`
+            rows.push({
+               title: x.subject,
+               rowId: `${isPrefix}gc ${x.id}`,
+               description: `[ ${v.stay ? 'FOREVER' : (v.expired == 0 ? 'NOT SET' : Func.timeReverse(v.expired - new Date() * 1))}  | ${x.participants.length} | ${(v.mute ? 'OFF' : 'ON')} | ${moment(v.activity).format('DD/MM/YY HH:mm:ss')} ]`
+            })
          } else {
             global.db.groups.push({
                jid: x.id,
@@ -40,8 +40,10 @@ exports.run = {
             })
          }
       })
-      caption += `${global.footer}`
-      m.reply(caption)
+      client.sendList(m.chat, '', `Bot has joined to ${groups.length} groups. 🍟`, '', 'Tap!', [{
+         rows
+      }], m)
    },
    cache: true,
    location: __filename
+}
